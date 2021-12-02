054e311d098f6d06e6babe3f5e7fd0cdb41f0c99
MaximeVerger <vergermaxime.sio@gmail.com>
2021-12-02 18:43:43 +0100

    modification of selection.py

--- selection.py
+++ selection.py
@@ -8,15 +8,14 @@ array2 = [0,1,2,6,1,0,15,12,3,8,9]
 array3 = [77,75,72,70,60,52,41,3,2,21,21,10]
 
 def selection_sort(array: list[int]) -> list[int]:
-    #on parcourt tous les éléments du tableau en gardant la valeur 
-    for i, value in enumerate(array):
+    #on parcourt tous les éléments du tableau
+    for i in range(len(array)):
         #pour chaque élement on parcours la suite du tableau
         permut_index = i
-        for j in range(i+1, len(array)):
-            #si on en trouve un plus petit on le celui conservé on
-            #le prend à la place
-            if value > array[j]:
-                value = array[j]
+        for j in range(i, len(array)):
+            #si on en trouve un plus petit que celui conservé on
+            #prend son index à la place
+            if array[j] < array[permut_index]:
                 permut_index = j
         #une fois à la fin on permutte le plus petit nombre trouvé avec
         #le premier sélectionné
