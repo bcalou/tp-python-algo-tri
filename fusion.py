bcb5b0f88f554c18e32e02b3a5b22886e464426d
Victor Mommalier <victormommalier1@gmail.com>
2021-12-05 18:51:09 +0100

    [FIX]: ajout du dernier élément fixé

--- fusion.py
+++ fusion.py
@@ -2,7 +2,7 @@ import random
 import time
 
 def sort(tab : list[int]) -> list[int]:
-    if len(tab) == 1: return tab
+    if len(tab) <= 1: return tab
 
     tabLeft: list[int] = sort(tab[:int(len(tab)/2)])
     tabRight: list[int] = sort(tab[int(len(tab)/2):])
@@ -21,17 +21,18 @@ def merge(tab1: list[int], tab2: list[int]) -> list[int]:
             tabFinal.append(tab2[0])
             tab2.pop(0)
 
-    tabFinal.append(tab1[0]) if len(tab1) > 0 else tabFinal.append(tab2[0])
+    tabFinal += (tab1,tab2)[len(tab2) > 0]
 
     return tabFinal
 
 
-# tabStart : list[int] = [1,12,6,4]
-# tabTest : list[int] = [3,9,2,28,1,12,6,4]
-# tabFinal: list[int] = sort(tabTest) #merge(tabStart, tabTest)
-# print("----------------------------------\n")
-# print(tabFinal)
-# print("\n----------------------------------")
+# for _ in range(0,5):
+#     tabTest : list[int] = [random.randint(0, 100) for _ in range(10)]
+#     tabFinal: list[int] = sort(tabTest)
+#     print("----------------------------------\n")
+#     print(tabTest)
+#     print(tabFinal)
+#     print("\n----------------------------------")
 
 tableau: list[int] = [random.randint(0, 100) for _ in range(1000)]
 start: float = time.time()
