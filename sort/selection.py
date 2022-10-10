960bd7c8129a544e1ffe152d72328fc2684649b7
Antoine <antoine.martin885@gmail.com>
2022-10-10 16:37:32 +0200

    Removed non efficient list in selection algorithm

--- sort/selection.py
+++ sort/selection.py
@@ -7,17 +7,18 @@ def sort(array: list[int]) -> list[int]:
     """Sort the list with a selection algorithm"""
     for reference_index, reference_number in enumerate(array):
         # Our reference number is the smallest at the beginning
-        smallest_number: [int, int] = [reference_index, reference_number]
+        smallest_number_index: int = reference_index
 
         # Comparing the reference with every other number in the list after the
         # reference
         for j in range(reference_index, len(array)):
-            if array[j] < smallest_number[1]:
-                smallest_number = [j, array[j]]
+            if array[j] < array[smallest_number_index]:
+                smallest_number_index = j
 
         # Exchange the smallest number found with the reference number in the
         # list
-        array[reference_index], array[smallest_number[0]] = (smallest_number[1],
-                                                             reference_number)
+        (array[reference_index],
+         array[smallest_number_index]) = (array[smallest_number_index],
+                                          reference_number)
 
     return array
