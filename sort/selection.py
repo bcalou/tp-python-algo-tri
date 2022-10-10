2668adf39a0d7e94833cb912214cd783a68348a0
Antoine <antoine.martin885@gmail.com>
2022-10-10 16:24:30 +0200

    Updated selection sort

--- sort/selection.py
+++ sort/selection.py
@@ -6,19 +6,18 @@ Selection sort
 def sort(array: list[int]) -> list[int]:
     """Sort the list with a selection algorithm"""
     for reference_index, reference_number in enumerate(array):
-        # Our reference number
+        # Our reference number is the smallest at the beginning
         smallest_number: [int, int] = [reference_index, reference_number]
 
-        # Compare the reference with every other number in the list after the
+        # Comparing the reference with every other number in the list after the
         # reference
-        for compared_number_index, j in enumerate(array[reference_index + 1:]):
-            if j < smallest_number[1]:
-                smallest_number = [compared_number_index + reference_index + 1,
-                                   j]
+        for j in range(reference_index, len(array)):
+            if array[j] < smallest_number[1]:
+                smallest_number = [j, array[j]]
 
         # Exchange the smallest number found with the reference number in the
         # list
-        array[reference_index] = smallest_number[1]
-        array[smallest_number[0]] = reference_number
+        array[reference_index], array[smallest_number[0]] = (smallest_number[1],
+                                                             reference_number)
 
     return array
