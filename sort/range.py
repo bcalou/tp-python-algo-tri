0d903bedbd16834e70726f10570f93fa826da3b4
Antoine <antoine.martin885@gmail.com>
2022-10-10 16:26:24 +0200

    Added generate_array_of_number and code to estimate time

--- sort/range.py
+++ sort/range.py
@@ -1,7 +1,24 @@
 """
 Number generator
 """
+import random
+import time
 
 
 def generate_array_of_number(array_size: int) -> list[int]:
-    return []
+    return [random.randint(0, 100) for i in range(array_size)]
+
+
+list_size: int = 1000000
+execution_times: dict[int, float] = {}
+for i in range(10):
+    start: float = time.time()
+
+    array: list[int] = generate_array_of_number(list_size)
+
+    end: float = time.time()
+    execution_times[list_size] = (end - start)
+    list_size += 1000000
+
+# {Number of element in the list: Time to do the task in seconds}
+print(execution_times)
