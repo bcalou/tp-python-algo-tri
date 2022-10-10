eb84b088be3257c0c86c175590d2e502b36beea9
Roucool_PvP <lucastag77@gmail.com>
2022-10-10 14:11:25 +0200

    Selection

--- sort/selection.py
+++ sort/selection.py
@@ -2,6 +2,27 @@
 Selection sort
 """
 
+import random
+
+def main():
+    """Main function
+    """
+    array = [random.randint(0, 100) for _ in range(10)]
+    print(array)
+    print(sort(array))
 
 def sort(array: list[int]) -> list[int]:
-    return []
+    """Sorts an array using the selection sort algorithm
+    """
+    for i in range(len(array)):
+        min_index = i
+        for j in range(i + 1, len(array)):
+            if array[j] < array[min_index]:
+                min_index = j
+        array[i], array[min_index] = array[min_index], array[i]
+
+    return array
+
+
+if __name__ == "__main__":
+    main()
