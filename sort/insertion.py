48e9a2fcd45246d9c3f264d91b3f1431f7c76196
Antoine <antoine.martin885@gmail.com>
2022-10-11 16:25:56 +0200

    Added insertion sort algorithm

--- sort/insertion.py
+++ sort/insertion.py
@@ -4,4 +4,21 @@ Insertion sort
 
 
 def sort(array: list[int]) -> list[int]:
-    return []
+    for reference_index in range(1, len(array)):
+        # The number we are testing
+        reference_value = array[reference_index]
+        # The index of the number before the tested number
+        previous_value_index = reference_index - 1
+
+        while reference_value < array[previous_value_index] and \
+                previous_value_index >= 0:
+            # If the number we are testing is smaller than its previous number,
+            # we put the previous value at the number position
+            array[previous_value_index + 1] = array[previous_value_index]
+            previous_value_index -= 1
+
+        # Our reference number is now greater than the number at
+        # previous_value_index, so we put it after
+        array[previous_value_index + 1] = reference_value
+
+    return array
