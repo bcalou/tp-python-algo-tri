172f6e8b8a657499efcc21fd56c736adb71dbc16
Antoine <antoine.martin885@gmail.com>
2022-10-11 11:44:34 +0200

    Refactor selection algorithm

--- sort/selection.py
+++ sort/selection.py
@@ -11,14 +11,13 @@ def sort(array: list[int]) -> list[int]:
 
         # Comparing the reference with every other number in the list after the
         # reference
-        for j in range(reference_index, len(array)):
-            if array[j] < array[smallest_number_index]:
-                smallest_number_index = j
+        for tested_number_index in range(reference_index, len(array)):
+            if array[tested_number_index] < array[smallest_number_index]:
+                smallest_number_index = tested_number_index
 
         # Exchange the smallest number found with the reference number in the
         # list
-        (array[reference_index],
-         array[smallest_number_index]) = (array[smallest_number_index],
-                                          reference_number)
+        array[reference_index], array[smallest_number_index] = \
+            array[smallest_number_index], reference_number
 
     return array
