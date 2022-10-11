b639ef87dec80269124ddc1d42c3eddddc38dde0
82L <16747176+82L@users.noreply.github.com>
2022-10-11 09:34:48 +0200

    Insertion sort

--- sort/insertion.py
+++ sort/insertion.py
@@ -4,4 +4,16 @@ Insertion sort
 
 
 def sort(array: list[int]) -> list[int]:
-    return []
+    """Sort by insertion"""
+    # position: int = 0
+    print(array)
+    for array_index in range(0, len(array)):
+        element_to_sort = array[array_index]
+        for subarray_index in range(array_index - 1, -1, -1):
+            if element_to_sort < array[subarray_index] and (subarray_index == 0 or
+                                                            element_to_sort >= array[subarray_index - 1]):
+                array.pop(array_index)
+                array.insert(subarray_index, element_to_sort)
+                print(array)
+                break
+    return array
