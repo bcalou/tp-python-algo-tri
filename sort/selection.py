10c6c44b39d26bc576d149d135af49b6c68155c7
82L <16747176+82L@users.noreply.github.com>
2022-10-11 09:34:14 +0200

    Selection debug

--- sort/selection.py
+++ sort/selection.py
@@ -9,7 +9,7 @@ def sort(array: list[int]) -> list[int]:
     start: float = time.time()
     # Go through each element of list
     array_length: int = len(array)
-    for current_index, element in enumerate(array):
+    for current_index in range(0, array_length):
         min_element_index = current_index
         # For each posterior element of the list, we try to find if the element is inferior.
         # We must pass by a secondary sublist index,
@@ -18,7 +18,7 @@ def sort(array: list[int]) -> list[int]:
             if array[min_element_index] > array[sublist_index]:
                 min_element_index = sublist_index
         # After finding the min, we exchange it with our current position
-        array[current_index], array[min_element_index] = array[min_element_index], element
+        array[current_index], array[min_element_index] = array[min_element_index], array[current_index]
     end: float = time.time()
     print("Temps écoulé :", end - start)
     return array
