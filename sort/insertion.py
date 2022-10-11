8ae97b1067fb457783391ab7bd1404bcbf43be2f
82L <16747176+82L@users.noreply.github.com>
2022-10-11 09:56:22 +0200

    Insertion add time + change var names

--- sort/insertion.py
+++ sort/insertion.py
@@ -1,19 +1,25 @@
 """
 Insertion sort
 """
+import time
 
 
 def sort(array: list[int]) -> list[int]:
     """Sort by insertion"""
-    # position: int = 0
-    print(array)
-    for array_index in range(0, len(array)):
-        element_to_sort = array[array_index]
-        for subarray_index in range(array_index - 1, -1, -1):
-            if element_to_sort < array[subarray_index] and (subarray_index == 0 or
-                                                            element_to_sort >= array[subarray_index - 1]):
-                array.pop(array_index)
-                array.insert(subarray_index, element_to_sort)
-                print(array)
+    start: float = time.time()
+
+    # For each index of the array
+    for current_index in range(0, len(array)):
+        element_to_sort = array[current_index]
+        # For each element before the element, we compare it to insert it
+        for sub_index in range(current_index - 1, -1, -1):
+            # If the element is inferior to current sub index and superior to the sub index before,
+            # exception if it equals 0
+            if element_to_sort < array[sub_index] and (sub_index == 0 or
+                                                       element_to_sort >= array[sub_index - 1]):
+                array.pop(current_index)
+                array.insert(sub_index, element_to_sort)
                 break
+    end: float = time.time()
+    print("Temps écoulé :", end - start)
     return array
