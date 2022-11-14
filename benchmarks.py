9d83172fc9b913b69a612650b613661848bd02a1
bcalou <bastien.calou@gmail.com>
2022-11-14 14:00:35 +0100

    add fusion and python sort to benchmark

--- benchmarks.py
+++ benchmarks.py
@@ -5,17 +5,24 @@ Timing benchmarks of the sorting functions
 from sort.timer import test_sort_function, test_array_generation_function
 from sort.selection import sort as selection_sort
 from sort.insertion import sort as insertion_sort
+from sort.fusion import sort as fusion_sort
 
 
 def main():
     """Get execution time for several functions"""
-    print("Génération d'un tableau de nombres aléatoires :")
+    print("\nGénération d'un tableau de nombres aléatoires")
     test_array_generation_function(1_000_000)
 
-    print("Tri par séléction")
+    print("\nTri par séléction")
     test_sort_function(selection_sort, 1_000)
 
-    print("Tri par insertion")
+    print("\nTri par insertion")
     test_sort_function(insertion_sort, 1_000)
+
+    print("\nTri par fusion")
+    test_sort_function(fusion_sort, 1_000)
+
+    print("\nTri natif de python")
+    test_sort_function(sorted, 1000)
 
 main()
--- sort/fusion.py
+++ sort/fusion.py
@@ -6,7 +6,7 @@ Fusion sort
 def sort(array: list[int]) -> list[int]:
     """Sort the array with fusion sort"""
 
-    # If there are several elements
+    # If there are several elements in the array
     if len(array) > 1:
 
         # Split the array in two smaller parts and use recursion to sort them
@@ -28,7 +28,7 @@ def merge(left_part: list[int], right_part: list[int]) -> list[int]:
     # Keep track of where we are in both arrays
     left_index = right_index = 0
 
-    # While they're still values to look at both of the arrays
+    # While they're still values to look at in both of the arrays
     while left_index < len(left_part) and right_index < len(right_part):
 
         # Use value from the left array if it's the smallest
