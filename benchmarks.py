37059bafd9f6e9003c1e9c2b71b52e128c2bc631
bcalou <bastien.calou@gmail.com>
2022-11-14 13:33:25 +0100

    improve benchmark and timer files

--- benchmarks.py
+++ benchmarks.py
@@ -2,14 +2,17 @@
 Timing benchmarks of the sorting functions
 """
 
-from sort.timer import test_function_execution_times
-from sort.range import generate_array_of_number
+from sort.timer import test_sort_function, test_array_generation_function
+from sort.selection import sort as selection_sort
 
 
 def main():
     """Get execution time for several functions"""
     print("Génération d'un tableau de nombres aléatoires :")
-    test_function_execution_times(generate_array_of_number, 1_000_000)
+    test_array_generation_function(1_000_000)
+
+    print("Tri par séléction")
+    test_sort_function(selection_sort, 1_000)
 
 
 main()
--- sort/timer.py
+++ sort/timer.py
@@ -4,15 +4,25 @@ Timer functions
 
 from typing import Callable
 import time
+from sort.range import generate_array_of_number
 
 
-def test_function_execution_times(function: Callable, step: int):
-    """Test the given function 10 times with increasing values"""
-    for input_size in range(step, step * 10, step):
+def test_array_generation_function(step: int):
+    """Test the array generation 10 times with increasing array sizes"""
+    for array_size in range(step, step * 11, step):
         execution_time: float = get_execution_time(
-          lambda size=input_size: function(size)
+            lambda size=array_size: generate_array_of_number(size)
         )
-        print(f"{input_size} entrées : {execution_time}s")
+        print(f"{array_size} entrées : {execution_time}s")
+
+
+def test_sort_function(function: Callable, step: int):
+    """Test the given sort function 10 times with increasing array sizes"""
+    for array_size in range(step, step * 11, step):
+        execution_time: float = get_execution_time(
+          lambda size=array_size: function(generate_array_of_number(size))
+        )
+        print(f"{array_size} entrées : {execution_time}s")
 
 
 def get_execution_time(function: Callable) -> float:
