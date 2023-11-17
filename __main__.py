a83ddc10921931806734a4ea770f819d91bfe7ca
MZASU <theo.thenault@gmail.com>
2023-11-17 15:56:00 +0100

    Tests et résulats trie par selection

--- __main__.py
+++ __main__.py
@@ -1,17 +1,16 @@
 from sort.range import generate_array_of_number
+from sort.selection import sort
+import time
 
 
 def main():
-    generate_array_of_number(1_000_000)
-    generate_array_of_number(2_000_000)
-    generate_array_of_number(3_000_000)
-    generate_array_of_number(4_000_000)
-    generate_array_of_number(5_000_000)
-    generate_array_of_number(6_000_000)
-    generate_array_of_number(7_000_000)
-    generate_array_of_number(8_000_000)
-    generate_array_of_number(9_000_000)
-    generate_array_of_number(10_000_000)
+    for sample_size in range(1000, 11_000, 1000):
+        sample: list[int] = generate_array_of_number(sample_size)
+        start: float = time.time()
+        sorted_sample: list[int] = sort(sample)
+        end: float = time.time()
+        print(f"Trie par insertion: taille tableau={sample_size}, \
+              temps={end-start}s")
 
 
 main()
--- sort/selection.py
+++ sort/selection.py
@@ -29,3 +29,16 @@ def sort(array: list[int]) -> list[int]:
         array.insert(number_of_sorted_numbers, smallest_number)
 
     return array
+
+
+# Résultats
+# Trie par insertion: taille tableau=1000, temps=0.10017824172973633s
+# Trie par insertion: taille tableau=2000, temps=0.2648162841796875s
+# Trie par insertion: taille tableau=3000, temps=0.5844788551330566s
+# Trie par insertion: taille tableau=4000, temps=1.1860754489898682s
+# Trie par insertion: taille tableau=5000, temps=1.6009154319763184s
+# Trie par insertion: taille tableau=6000, temps=2.032719135284424s
+# Trie par insertion: taille tableau=7000, temps=2.8216004371643066s
+# Trie par insertion: taille tableau=8000, temps=3.711418867111206s
+# Trie par insertion: taille tableau=9000, temps=4.674601316452026s
+# Trie par insertion: taille tableau=10000, temps=5.86929988861084s
