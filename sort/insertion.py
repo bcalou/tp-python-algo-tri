38b37057bbf29de45add88fef1031390377d4a2f
AITOUFEL <badro-5@hotmail.com>
2023-11-25 14:54:12 +0100

    KHAZZANI Badr

--- sort/insertion.py
+++ sort/insertion.py
@@ -1,2 +1,30 @@
-def sort(array: list[int]) -> list[int]:
+import time
+from typing import List
+import range as range_array 
+# Assuming you have a module named 'range_array' for generating arrays
+
+def main():
+    '''Function to evaluate the algorithm complexity'''
+    for size in range(1, 11):
+        array: List[int] = range_array.generate_array_of_number(size * 1000)
+        start_time: float = time.time()
+        sort(array)
+        end_time: float = time.time()
+        print("Taille de tableau : ", size * 1000,
+              " Temps écoulé :", end_time - start_time)
+
+def sort(array: List[int]) -> List[int]:
+    for old_index in range(1, len(array)):
+        current_value = array[old_index]
+        index_to_place = old_index - 1
+
+        while index_to_place >= 0 and current_value < array[index_to_place]:
+            array[index_to_place + 1] = array[index_to_place]
+            index_to_place -= 1
+
+        array[index_to_place + 1] = current_value
+
     return array
+
+if __name__ == "__main__":
+    main()
--- sort/range.py
+++ sort/range.py
@@ -1,2 +1,19 @@
+import random
+import time
+
+def main():
+    '''Function to evaluate the algorithm complexity'''
+    for size in range(1, 11):
+        start_time = time.time()
+        array_size = size * 1_000_000
+        generate_array_of_number(array_size)
+        end_time = time.time()
+        print("Taille de tableau : ", array_size,
+              " Temps écoulé :", end_time - start_time)
+
 def generate_array_of_number(array_size: int) -> list[int]:
-    return []
+    """Generate an array of random numbers."""
+    return [random.randint(0, 100) for _ in range(array_size)]
+
+if __name__ == "__main__":
+    main()
