d6ce23dbf5c74e34f897ec522f3c5142a5c00d4a
DESKTOP-ISVPJFT\Mathieu <mat.chico86.mc@gmail.com>
2023-11-27 03:27:59 +0100

    TP Tris Mathieu Chicoine

--- sort/insertion.py
+++ sort/insertion.py
@@ -1,10 +1,16 @@
 def sort(array: list[int]) -> list[int]:
-    """Tri par algorithme d'insersion"""
+    """Tri par algorithme d'insertion"""
     len_list = len(array)
     for index in range(1, len_list):
         value = array[index]
-        j = index - 1
-        while j >= 0 and array[j] > value:
-            array[j + 1], array[j] = array[j], value
-            j -= 1
+        start = index - 1
+
+        # Trouver le bon emplacement en déplaçant les éléments plus grands
+        while start >= 0 and array[start] > value:
+            array[start + 1] = array[start]
+            start -= 1
+
+        # Insérer la valeur à la bonne position
+        array[start + 1] = value
+
     return array
